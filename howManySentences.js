5fa8c9739c2fead9fb2effc2a5dff19362e69215
Jon Phelps <jonathanp.phelps@gmail.com>
2023-02-03 13:08:47 -0500

    added cases for other punctuation, plus notes for future tweaks

--- howManySentences.js
+++ howManySentences.js
@@ -6,12 +6,22 @@ function previewFile() {
     const reader = new FileReader();
   
     reader.addEventListener("load", () => {
-      // this will then display a text file
-      let text = reader.result.split('.').length
-      content.innerText = text;
+      let periods = reader.result.split('. ')
+      let questions = reader.result.split('?')
+      let exclamations = reader.result.split('!')
+      let count = periods.length + questions.length + exclamations.length
+      //count each instance of punctuaction parameters, '. ' should avoids triple count on elipses
+      console.log(periods[0])
+      console.log(questions[0])
+      console.log(exclamations[0])
+      //console.logs for manual test
+      content.innerText = count;
     }, false);
   
     if (file) {
       reader.readAsText(file);
     }
   }
+
+
+//use one .split(regex) to simplify code?
